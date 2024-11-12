b9836133cc7b826fed177c7987bd753a41d85858
lnemt <alenemt@yandex.ru>
2024-11-12 06:06:02 +0300

    add config management

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 import time
 import csv
+import random
+from collections import namedtuple
 
 # import sys
 # import os
+OPERATIONS = ("+", "-", "*")
 
 
-def start_session(start_time):
+def isoperation(string: str):
+    return string in OPERATIONS
+
+
+def start_session(start_time, game_config):
     while time.time() - start_time <= 20:
-        x = int(input())
+        x = random.randint(0, 10**game_config.first_num)
+        y = random.randint(0, 10**game_config.second_num)
+
+        answ = x
+        res = int(input())
         print(x)
 
     print("Your 5 min session has ran out! Well done")
@@ -19,4 +30,18 @@ def start_session(start_time):
 if __name__ == "__main__":
     start_time = time.time()
 
-    start_session(start_time)
+    print('Welcome to the ultimate "git gud counting" game!')
+    print("Please input 3 values: (num size, num size, operation)")
+    config_tuple = namedtuple("Config", ["first_num", "second_num", "operation"])
+    game_config = config_tuple._make(str(input()).split(sep=None))
+
+    print(game_config.__repr__())
+    # if all(
+    #     game_config..isnumeric(),
+    #     game_config[1].isnumeric(),
+    #     isoperation(game_config[2]),
+    # ):
+    #     print("Wrong input, try again.")
+    #     exit(1)
+
+    start_session(start_time, game_config)
